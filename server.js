ae4a277c071d44927440269249e53ba4bc9e9c78
akghosh111 <ghoshkiran2017@gmail.com>
2026-06-11 21:57:37 +0530

    add management api

--- server.js
+++ server.js
@@ -1,12 +1,75 @@
 import express from "express";
+import Docker from "dockerode";
+
+const docker = new Docker();
+
+function pullImagePromisified(image, tag) {
+    return new Promise((res, rej) => {
+        docker.pull(`${image}:${tag}`, {}, (err) => {
+            if(err){
+                rej(err);
+            } else {
+                return res(true);
+            }
+        })
+    })
+}
 
 const managementApp = express();
 
-const MANAGEMENT_API_PORT = process.env.MANAGEMENT_API_PORT ?? 8080
+managementApp.use(express.json());
+
+const MANAGEMENT_API_PORT = process.env.MANAGEMENT_API_PORT ?? 8080;
+const REVERSE_PROXY_HOST = process.env.REVERSE_PROXY_HOST ?? "localhost";
 
 managementApp.get("/", (req, res) => {
     return res.json({ status: "Management APIs are up and running" });
 });
+
+managementApp.post("/container", async (req, res) => {
+    const { image, tag } = req.body;
+    const systemImages =  docker.listImages();
+    let isExistingImage = false;
+
+    for (const systemImage of systemImages) {
+        for (const systemTag of systemImage.RepoTags) {
+            if(systemTag === `${image}:${tag}`) {
+                isExistingImage = true;
+                break;
+            }
+            
+        }
+
+        if (isExistingImage) break;
+    }
+
+    if(!isExistingImage) {
+        await pullImagePromisified(image, tag);
+    }
+
+    const container = await docker.createContainer({
+        Image: `${image}:${tag}`,
+        HostConfig: {
+            AutoRemove: true,
+        },
+    });
+
+    await container.start();
+
+    // TODO: Also connect this to the host network
+
+    const inspect = await container.inspect();
+
+    return res.json({
+        status: "success",
+        data: {
+            containerName: inspect.Name,
+            domain: `${inspect.Name}.${REVERSE_PROXY_HOST}`
+        }
+    });
+
+
+} )
 
 managementApp.listen(MANAGEMENT_API_PORT, () => {
     console.log(`ManagementAPI is running on PORT ${MANAGEMENT_API_PORT}`);
